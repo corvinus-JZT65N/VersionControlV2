228c89a89d77a724073612e3ee61977fa5134043
Gergely Kiss <gergely.kiss3@stud.uni-corvinus.hu>
2021-10-08 21:15:15 +0200

    EltűnőAdatokTeszt

--- A HELYES/UserMaintenance/UserMaintenance/Form1.cs
+++ A HELYES/UserMaintenance/UserMaintenance/Form1.cs
@@ -62,7 +62,7 @@ namespace UserMaintenance
                 sw.Close();
             }
             
-
+            //sadf
         }
 
         private void button3_Click(object sender, EventArgs e)
