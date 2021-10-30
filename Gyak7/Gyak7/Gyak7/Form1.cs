d9186a55c2903eabd97905f9226f5c54f9f91e84
Gergely Kiss <gergely.kiss3@stud.uni-corvinus.hu>
2021-10-30 15:58:54 +0200

    4. Feladat

--- Gyak7/Gyak7/Gyak7/Form1.cs
+++ Gyak7/Gyak7/Gyak7/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Gyak7.Entities;
+using Gyak7.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,31 @@ namespace Gyak7
 {
     public partial class Form1 : Form
     {
+
+        List<RateData> Rates = new List<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates.ToList();
+            Feladat3();
+            
+        }
+
+        private void Feladat3()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            Console.WriteLine(result);
         }
     }
 }
