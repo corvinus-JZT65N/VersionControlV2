39211625967f2e9b40fb743b4e7b88cfc4887967
Gergely Kiss <gergely.kiss3@stud.uni-corvinus.hu>
2021-11-13 16:01:44 +0100

    2. Feladat

--- Gyak8/Gyak8/Form1.cs
+++ Gyak8/Gyak8/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gyak8.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,49 @@ namespace Gyak8
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+            mainPanel.Width = Width;
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            int maxPosition = 0;
+            foreach (var item in _balls)
+            {
+                item.MoveBall();
+                if (item.Left > maxPosition)
+                {
+                    maxPosition = item.Left;
+                }
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
